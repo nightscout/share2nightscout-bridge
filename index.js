82c43a0769fbaeebe4dc693f4fb68ff7e82abb34
Ben West <bewest@gmail.com>
2015-03-13 18:26:19 -0700

    push to ns

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var request = require('request');
 var qs = require('querystring');
+var crypto = require('crypto');
 
 
 var Defaults = {
@@ -11,6 +12,7 @@ var Defaults = {
 , 'content-type': 'application/json'
 , LatestGlucose: "https://share1.dexcom.com/ShareWebServices/Services/Publisher/ReadPublisherLatestGlucoseValues"
 // ?sessionID=e59c836f-5aeb-4b95-afa2-39cf2769fede&minutes=1440&maxCount=1"
+, nightscout_upload: '/api/v1/entries.json'
 };
 
     // -H "Accept: application/json" -H "Content-Length: 0"
@@ -63,12 +65,49 @@ function do_everything (opts, then) {
   var login_opts = opts.login;
   var fetch_opts = opts.fetch;
   authorize(login_opts, function (err, res, body) {
+
     fetch_opts.sessionID = body;
     fetch(fetch_opts, function (err, res, glucose) {
-      then(glucose);
+      then(err, glucose);
 
     });
   });
+
+}
+
+function dex_to_entry (d) {
+/*
+[ { DT: '/Date(1426292016000-0700)/',
+    ST: '/Date(1426295616000)/',
+    Trend: 4,
+    Value: 101,
+    WT: '/Date(1426292039000)/' } ]
+*/
+  var regex = /\((.*)\)/;
+  var wall = parseInt(d.WT.match(regex)[1]);
+  var date = new Date(wall);
+  var entry = {
+    sgv: d.Value
+  , date: wall
+  , dateString: date.toISOString( )
+  , trend: d.Trend
+  , device: 'share2'
+  , type: 'sgv'
+  // , device: 'dexcom'
+  };
+  return entry;
+}
+
+function report_to_nightscout (opts, then) {
+  var shasum = crypto.createHash('sha1');
+  var hash = shasum.update(opts.API_SECRET);
+  var headers = { 'api-secret': shasum.digest('hex')
+                , 'Content-Type': Defaults['content-type']
+                , 'Accept': Defaults.accept };
+  var url = opts.endpoint + Defaults.nightscout_upload;
+  var req = { uri: url, body: opts.entries, json: true, headers: headers, method: 'POST'
+            , rejectUnauthorized: false }; 
+  return request(req, then);
 
 }
 
@@ -78,7 +117,10 @@ if (!module.parent) {
     accountName: process.env['DEXCOM_ACCOUNT_NAME']
   , password: process.env['DEXCOM_PASSWORD']
   };
-  console.log(args);
+  var ns_config = {
+    API_SECRET: process.env['API_SECRET']
+  , endpoint: process.env['NS']
+  };
   switch (args[0]) {
     case 'login':
       authorize(config, console.log.bind(console, 'login'));
@@ -92,7 +134,18 @@ if (!module.parent) {
         login: config
       , fetch: { }
       };
-      do_everything(meta, console.log.bind(console, 'EVERYTHING'));
+      // do_everything(meta, console.log.bind(console, 'EVERYTHING'));
+      do_everything(meta, function (err, glucose) {
+        console.log('huh', arguments);
+        if (glucose) {
+          var entries = glucose.map(dex_to_entry);
+          console.log('Entries', entries);
+          if (ns_config.endpoint) {
+            ns_config.entries = entries;
+            report_to_nightscout(ns_config, console.log.bind(console, 'NIGHTSCOUT'));
+          }
+        }
+      });
       break;
 
   }
