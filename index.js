1806ae231001c63f7817fdff80b11ff1e2a4fb3c
Samuel <samuelalinder@glocalnet.net>
2016-02-04 11:28:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ var crypto = require('crypto');
 var Defaults = {
   "applicationId":"d89443d2-327c-4a6f-89e5-496bbb0317db"
 , "agent": "Dexcom Share/3.0.2.11 CFNetwork/711.2.23 Darwin/14.0.0"
-, login: 'https://share1.dexcom.com/ShareWebServices/Services/General/LoginPublisherAccountByName'
+, login: 'https://shareous1.dexcom.com/ShareWebServices/Services/General/LoginPublisherAccountByName'
 , accept: 'application/json'
 , 'content-type': 'application/json'
-, LatestGlucose: "https://share1.dexcom.com/ShareWebServices/Services/Publisher/ReadPublisherLatestGlucoseValues"
+, LatestGlucose: "https://shareous1.dexcom.com/ShareWebServices/Services/Publisher/ReadPublisherLatestGlucoseValues"
 // ?sessionID=e59c836f-5aeb-4b95-afa2-39cf2769fede&minutes=1440&maxCount=1"
 , nightscout_upload: '/api/v1/entries.json'
 , nightscout_battery: '/api/v1/devicestatus.json'
