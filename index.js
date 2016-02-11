eeade81b8dbbf0a1f79dbca3d6d90b56ebd6ddbf
Samuel <samuelalinder@glocalnet.net>
2016-02-11 16:42:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var crypto = require('crypto');
 var server = "share1.dexcom.com"
 var bridge = readENV('BRIDGE_SERVER')
     if (bridge.indexOf(".") > 1) {
-    server = readENV('BRIDGE_SERVER');
+    server = bridge;
    } 
     else if (readENV('BRIDGE_SERVER') == 'EU') {
         server = "shareous1.dexcom.com";
